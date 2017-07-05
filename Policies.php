b20336d7265852e1852bfba2b1c6a2d898dfb15a
SitharaWanigasooriya <sitharawanigasooriya@gmail.com>
2017-07-05 17:59:26 +0530

    Changes on Some Lines

--- Policies.php
+++ Policies.php
@@ -2,22 +2,26 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Policies</title>
+<title>Update Policies</title>
 	<link href="CSS/Policiess.css" type="text/css" rel="stylesheet">
+	<link href= "CSS/Remove Customer.css" type="text/css" rel="stylesheet">
 </head>
 
 <body>
 	<div id="Heading">
-		<h1>Policies<h1>
+		<h1> Update Policies<h1>
 	</div>
 	
-	<div id="Description">
-	<p>Currently use  the gassert No:453/6 and the date is 1987/05/12</p>
+	<div class="form_control">
+		<form action="Update Policies.php" method="post">
+			New Assesment tax rate:<input type="text" name="newAssesmentTax"/>
+			New Shop tax rate: <input type="text" name="newShopTax"/>
+		</form>
+		
 	</div>
 	
 	<div class="bttn_control">
 		<button id="Btn" type ="submit" name ="View Policies">View Policies</button>
-		<button id="Btn" type="submit" name="Update Policies">Update Policies</button>
 	</div>	
 </body>
 </html>
